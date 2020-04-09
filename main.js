503c2c4d7b3222d342b9d93997d8e9ceab17b0da
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2020-04-09 09:04:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ window.onload = function() {
 	[...document.getElementsByClassName('portfolio-cell')].forEach(function(v) {
 		v.onclick = function() {
 			var link = 'https://github.com/Avedati/';
-			if(this.id === 'd2rubikscube') { link += '2DRubiksCube'; }
-			if(this.id === 'd3rubikscube') { link += '3DRubiksCube'; }
+			if     (this.id === 'd2rubikscube') { link += '2DRubiksCube'; }
+			else if(this.id === 'd3rubikscube') { link += '3DRubiksCube'; }
+			else if(this.id === 'aicourse') { link = 'https://Avedati.github.io/TeachingWebsite#aicourse'; }
+			else if(this.id === 'gamedesigncourse') { link = 'https://Avedati.github.io/TeachingWebsite#gamedesigncourse'; }
 			else { link += this.id; }
 			window.open(link);
 		};
