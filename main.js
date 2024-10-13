1e74a5800d4a89714cd2c61b29d0d88a40d43a12
Abhi Vedati <ajvedati@gmail.com>
2024-10-12 17:22:19 -0700

    push october 2024 idea

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.github').click(function() {
+		window.open('https://github.com/avedati', '_blank');
+	});
+
+	$('.linkedin').click(function() {
+		window.open('https://www.linkedin.com/in/abhinav-vedati', '_blank');
+	});
+});
